6156cb9399319407c55309df2d1741cf966df6c1
TommyTheTerrible <81168766+TommyTheTerrible@users.noreply.github.com>
2025-05-10 18:34:46 -0400

    Revert "LLTextureFetchWorker::doWork - Add Decode Queue Check"
    This reverts commit cf5780e924a0cb1e70848407ac71eb536ff1fb2f.

--- indra/newview/lltexturefetch.cpp
+++ indra/newview/lltexturefetch.cpp
@@ -1915,14 +1915,6 @@ bool LLTextureFetchWorker::doWork(S32 param)
         LL_PROFILE_ZONE_NAMED_CATEGORY_TEXTURE("tfwdw - DECODE_IMAGE"); //<FS:Beq/> fix incorrect category
         static LLCachedControl<bool> textures_decode_disabled(gSavedSettings, "TextureDecodeDisabled", false);
 
-        // <3T:TommyTheTerrible> Do not send a request if decode queue is near the 1024 limit. 
-        if ((S32)LLAppViewer::getImageDecodeThread()->getPending() >= 1024)
-        {
-            // No room in decode queue, wait in state for opening. LLThreadSafeQueue default is 1024.
-            return false;
-        }
-        // </3T:TommyTheTerrible>)
-
         if (textures_decode_disabled)
         {
             // for debug use, don't decode
