5562ac0bfb28c309e1b4aef09041edf7611cc84c
TommyTheTerrible <81168766+TommyTheTerrible@users.noreply.github.com>
2025-05-18 18:45:08 -0400

    Revert "Remove updateFetch decode threadpool limits"
    This reverts commit 7fb3bd84b09f99782d4786f157a380372128bcf1.

--- indra/newview/llviewertexturelist.cpp
+++ indra/newview/llviewertexturelist.cpp
@@ -1374,15 +1374,21 @@ F32 LLViewerTextureList::updateImagesFetchTextures(F32 max_time)
     {
         mLastUpdateKey = LLTextureKey(last_imagep->getID(), (ETexListType)last_imagep->getTextureListType());
     }
+    S32 decodes_available = llmax((S32)LLAppViewer::getImageDecodeThread()->getThreadCount() * 4, update_count);
+    
+    S32 fetch_count  = 0;
 
     for (auto texture : mFetchingTextures)
     {
-        if (timer.getElapsedTimeF32() > max_time)
+        if (timer.getElapsedTimeF32() > max_time ||
+            fetch_count + 1 > decodes_available ||
+            LLAppViewer::getImageDecodeThread()->getPending() > 128)
             break;
 
         if (texture)
         {
             texture->updateFetch();
+            fetch_count++;
         }
     }
 
