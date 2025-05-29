ca2af1a89f57e7da21b306659bff68f2bcf842f5
TommyTheTerrible <81168766+TommyTheTerrible@users.noreply.github.com>
2025-05-29 14:26:40 -0400

    addTextureStats - Allow for Zero again except BOM
    Returned logic for allowing for zero virtual_size but added a conditional if a baked texture so they always are loaded with at least discard 5 to allow avatars to leave clouded status.

--- indra/newview/llviewertexture.cpp
+++ indra/newview/llviewertexture.cpp
@@ -782,7 +782,8 @@ bool LLViewerTexture::addTextureStats(F32 virtual_size) const
     // Adjust virtual_size to nearest power of two
     virtual_size = nearest_power_of_two(virtual_size);
     virtual_size = llmin(virtual_size, LLViewerFetchedTexture::sMaxVirtualSize);
-    //virtual_size = virtual_size * (virtual_size >= 2); // Nearest power of 2 to 0 is 1, so we need to catch it.
+    virtual_size = virtual_size * (virtual_size >= 2); // Nearest power of 2 to 0 is 1, so we need to catch it.
+    virtual_size += (mBoostLevel == LLGLTexture::BOOST_AVATAR_BAKED); // Adding 1 if baked texture so never 0 and always loads (so avatar clouding finishes)
     bool needs_update = (mMaxVirtualSize != virtual_size);
     mNeedsGLTexture = true;
     mMaxVirtualSize = virtual_size;
